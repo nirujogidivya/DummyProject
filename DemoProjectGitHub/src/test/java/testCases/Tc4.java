4b5caafc6ee6cd2e498f7286e2fbccf2010c6044
siva <siva@LAPTOP-2LF6BU76>
2023-06-07 16:09:00 +0530

    Tc4 commit

---
+++ DemoProjectGitHub/src/test/java/testCases/Tc4.java
@@ -0,0 +1,12 @@
+package testCases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc4 {
+ @Test
+ public void m4()
+ {
+	 Reporter.log("data updated",true);
+ }
+}
